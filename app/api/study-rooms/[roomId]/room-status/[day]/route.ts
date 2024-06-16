a9e05e58b13842c7d99ad1543465462058dc22a1
vaidyanath b <b.vaidyanath003@gmail.com>
2024-06-16 20:08:48 +0530

    aahg

--- app/api/study-rooms/[roomId]/room-status/[day]/route.ts
+++ app/api/study-rooms/[roomId]/room-status/[day]/route.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 export async function GET(req:NextApiRequest,context:any){
     const {room_id,day} = context.params
 
     //enable authotrization\
     const time = new Date()
-    time.setHours(0,0,0,0);
-    const today = time.toISOString();
-
+    time.setHours(0,0,0,0)
+    const tomorrow = new Date()
+    tomorrow.setHours(0,0,0,0)
+    tomorrow.setDate(time.getDate() + 1)
 
     const roomDay = await prisma.studyRoomDay.findUnique({
         where:{
@@ -21,10 +23,18 @@ export async function GET(req:NextApiRequest,context:any){
             studyRoom:true,
             StudyRoomQuiz:true,
             StudyRoomContent:true
-            
+
 
         }
     });
+    if (!roomDay)
+        return NextResponse.json({},{status:400 , statusText:"no room day got"})
+    const scheduled_date = new Date(roomDay.scheduled_date)
+
+
+
+    
+    NextResponse.json({...roomDay, isLive: scheduled_date > time && scheduled_date < tomorrow , isDead: scheduled_date > tomorrow},{status : 200});
 
     
 }
